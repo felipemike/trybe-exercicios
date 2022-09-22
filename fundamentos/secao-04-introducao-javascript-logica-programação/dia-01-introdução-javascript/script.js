6f73b9288a61dddee6f55bba823c73f05943b46f
felipemike <felipeclemente2012@gmail.com>
2022-09-22 15:07:23 -0300

    Create script.js

---
+++ fundamentos/secao-04-introducao-javascript-logica-programação/dia-01-introdução-javascript/script.js
@@ -0,0 +1,24 @@
+/*
+const myName = 'Felipe'
+const birthCity = 'Aracati'
+let birthYear = 1988;
+console.log(myName, birthCity, birthCity)
+birthCity = 'Fortaleza */
+
+/*const base = 5;
+const heigth = 8;
+const area =  base * heigth;
+
+const perimeter = base + heigth + area;
+
+console.log(perimeter)*/
+
+const nota = 50
+
+if (nota >= 80){
+    console.log('provado')
+} else if (nota < 80 && nota > 60){
+    console.log('Lista de espera')
+} else {
+    console.log('Reprovado')
+}
