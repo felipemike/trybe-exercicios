f50d33fffe243ab76c692ea60c91152a5ba2bd4d
felipemike <felipeclemente2012@gmail.com>
2022-09-22 15:07:19 -0300

    Create lição-07.js

---
+++ fundamentos/secao-04-introducao-javascript-logica-programação/dia-01-introdução-javascript/lição-07.js
@@ -0,0 +1,12 @@
+const a = 1
+const b = 2
+const c = 3
+let isValue = false
+
+if(a % 2 != 0 || b % 2 != 0 || c % 2 != 0){
+    isValue = true
+}else{
+    isValue = false
+};
+
+console.log(isValue)
