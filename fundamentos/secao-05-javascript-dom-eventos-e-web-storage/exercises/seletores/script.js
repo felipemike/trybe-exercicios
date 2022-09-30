b688fedeab911110f8769a5b249af752718833f3
felipemike <felipeclemente2012@gmail.com>
2022-09-29 21:02:11 -0300

    Create script.js

---
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/exercises/seletores/script.js
@@ -0,0 +1,4 @@
+let titulo = document.getElementById('header-container').style.backgroundColor = 'blue';
+
+let urgente = document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink'; 
+let gerente = document.getElementsByClassName('emergency-tasks')[2].style.backgroundColor = 'black';
