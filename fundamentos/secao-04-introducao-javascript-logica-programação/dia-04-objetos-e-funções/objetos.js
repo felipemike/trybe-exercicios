ca1bda4c97f581ef1e51bee9d53cec0e891708d9
felipemike <felipeclemente2012@gmail.com>
2022-09-29 21:02:05 -0300

    Create objetos.js

---
+++ fundamentos/secao-04-introducao-javascript-logica-programação/dia-04-objetos-e-funções/objetos.js
@@ -0,0 +1,19 @@
+
+//Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+//console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
