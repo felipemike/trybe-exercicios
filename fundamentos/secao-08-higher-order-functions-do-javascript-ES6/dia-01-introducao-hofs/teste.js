38f2bde128a85aa7d1bec4b88dbd5924a4636046
felipemike <felipeclemente2012@gmail.com>
2022-11-04 16:04:43 -0300

    Create teste.js

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-ES6/dia-01-introducao-hofs/teste.js
@@ -0,0 +1,19 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Adicione seu código aqui
+
+
+emailListInData.forEach((item, nome) => {
+  showEmailList(item);
+})
+
+// O email   
